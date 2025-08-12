ea1e8d2584e8af057eee598120b4eaa6f813b065
Jac Rees <jac.rees@vodafone.com>
2025-08-12 12:24:07 +0100

    feat: themes

--- src/assets/styled.d.ts
+++ src/assets/styled.d.ts
@@ -10,9 +10,7 @@ declare module "styled-components" {
       gray: string;
       "dark-gray": string;
       white: string;
-      black: string;
     };
-    fonts: string[];
     fontSizes: {
       sm: string;
       md: string;
@@ -20,38 +18,13 @@ declare module "styled-components" {
       xl: string;
     };
     fontStyles: {
-      fontFamily: string;
-      fontWeight: string;
       fontSize: string;
       letterSpacing: string;
       textTransform: string;
     };
     typography: {
-      fontFamily: string;
       fontWeightBold: number;
       fontWeightRegular: number;
-      headings: {
-        fontSize: string;
-        textTransform: string;
-        letterSpacing: string;
-        colorLightBg: string;
-        colorDarkBg: string;
-      };
-      body: {
-        fontSize: string;
-        colorLightBg: string;
-        colorDarkBg: string;
-      };
-      rating: {
-        valueSize: string;
-        labelSize: string;
-      };
-    };
-    stars: {
-      filledStarColor: string;
-      unfilledStarColor: string;
-      iconColorOnLight: string;
-      iconColorOnDark: string;
     };
     spacing: {
       xs: string;
--- src/assets/theme.tsx
+++ src/assets/theme.tsx
@@ -7,9 +7,7 @@ const theme = {
     white: "#FFFFFF",
     gray: "#E6E6E6",
     "dark-gray": "#444444",
-    black: "#000000",
   },
-  fonts: ["Inter", "Arial", "sans-serif"],
   fontSizes: {
     sm: "14px",
     md: "16px",
@@ -17,39 +15,20 @@ const theme = {
     xl: "22px",
   },
   fontStyles: {
-    fontFamily: "Inter, Arial, sans-serif",
-    fontWeight: "400",
     fontSize: "16px",
     letterSpacing: "0.135em",
     textTransform: "uppercase",
   },
   typography: {
-    fontFamily: "Inter, Arial, sans-serif",
     fontWeightBold: 700,
     fontWeightRegular: 400,
-    headings: {
-      fontSize: "22px",
-      textTransform: "uppercase",
-      letterSpacing: "0.135em",
-      colorLightBg: "#292F36",
-      colorDarkBg: "#FFFFFF",
-    },
-    body: {
-      fontSize: "16px",
-      colorLightBg: "#292F36",
-      colorDarkBg: "#FFFFFF",
-    },
+    
     rating: {
       valueSize: "18px",
       labelSize: "14px",
     },
   },
-  stars: {
-    filledStarColor: "#FFD100",
-    unfilledStarColor: "#E6E6E6",
-    iconColorOnLight: "#FFD100",
-    iconColorOnDark: "#FFFFFF",
-  },
+
   spacing: {
     xs: "4px",
     sm: "8px",
